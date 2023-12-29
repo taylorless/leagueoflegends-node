b3da5e5f12b8b4fef03fb82bea1a5343aedd79f0
lishanghang <630664429@qq.com>
2023-12-29 11:41:08 +0800

    init

--- routes/server.js
+++ routes/server.js
@@ -3,9 +3,13 @@ const router = express.Router();
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const { exec } = require("child_process");
 const https = require("https");
+const { log } = require("console");
 
 let allData = null;
-let currentUser = null;
+let currentSummonerInfo = null; //本地召唤师信息
+let MatchHistory = null; //比赛记录
+let currentSummonerRankInfo = null; //本地召唤师排位分数
+let currentSummonerChampionMastery = null; //本地召唤师英雄熟练度
 //拿去当前电脑客户端登录的lol用户基础信息
 const findLeagueClientCommandLine = async () => {
   return new Promise((resolve, reject) => {
@@ -23,7 +27,7 @@ const findLeagueClientCommandLine = async () => {
   });
 };
 
-const getRiotData = async (url, method = "GET") => {
+const getRiotData = async (url, method = "GET", params) => {
   //通过正则匹配拿到token和port
   const token = allData.match(/remoting-auth-token=(.*?)["'\s]/)[1];
   const port = allData.match(/--app-port=(.*?)["'\s]/)[1];
@@ -61,8 +65,28 @@ const main = async () => {
   try {
     const leagueClientCommandline = await findLeagueClientCommandLine();
     // console.log("LeagueClientUx.exe 命令行参数:", leagueClientCommandline);
-    currentUser = await getRiotData("/lol-summoner/v1/current-summoner");
-    // console.log("当前user信息为:", currentUser);
+
+    // 查询本地召唤师信息
+    currentSummonerInfo = await getRiotData(
+      "/lol-summoner/v1/current-summoner"
+    );
+
+    const { puuid, accountId, summonerId } = currentSummonerInfo;
+
+    //根据accountID查询战绩
+    MatchHistory = await getRiotData(
+      `/lol-match-history/v1/products/lol/${summonerId}/matches?begIndex=0&endIndex=1`
+    );
+
+    //查询本地召唤师排位分数
+    currentSummonerRankInfo = await getRiotData(
+      "/lol-ranked/v1/current-ranked-stats"
+    );
+
+    //查询本地召唤师英雄熟练度
+    currentSummonerChampionMastery = await getRiotData(
+      `/lol-collections/v1/inventories/${puuid}/champion-mastery`
+    );
   } catch (error) {
     console.error("发生错误:", error);
   }
@@ -71,8 +95,25 @@ const main = async () => {
 main();
 
 //node定义接口
-router.get("/api/getCurrentUserInfo", function (req, res, next) {
-  res.send(currentUser);
+
+// 查询本地召唤师信息
+router.get("/api/getCurrentSummoner", function (req, res, next) {
+  res.send(currentSummonerInfo);
+});
+
+//根据召唤师id查询比赛
+router.get("/api/getMatchHistoryByAccountId", function (req, res, next) {
+  res.send(MatchHistory);
+});
+
+//查询本地召唤师排位分数
+router.get("/api/getCurrentSummonerRankInfo", function (req, res, next) {
+  res.send(currentSummonerRankInfo);
+});
+
+//查询本地召唤师英雄熟练度
+router.get("/api/getCurrentSummonerChampionMastery", function (req, res, next) {
+  res.send(currentSummonerChampionMastery);
 });
 
 module.exports = router;
