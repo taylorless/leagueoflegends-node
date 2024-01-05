92ef17d41b85a23b456840dddad8c4de3e1636b4
lishanghang <630664429@qq.com>
2024-01-05 13:59:58 +0800

    init

--- routes/server.js
+++ routes/server.js
@@ -25,40 +25,42 @@ const findLeagueClientCommandLine = async () => {
 };
 
 const getRiotData = async (url, method = "GET", params) => {
-  if (allData == null) return false;
+  if (!allData) return false;
 
-  //通过正则匹配拿到token和port
-  const token = allData.match(/remoting-auth-token=(.*?)["'\s]/)[1];
-  const port = allData.match(/--app-port=(.*?)["'\s]/)[1];
-  const auth = Buffer.from(`riot:${token}`).toString("base64");
-  const headers = {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-    Authorization: `Basic ${auth}`,
-  };
-  const options = {
-    hostname: "127.0.0.1",
-    port: port,
-    path: url,
-    method: method,
-    headers: headers,
-    params: params,
-  };
-  return new Promise((resolve, reject) => {
-    const req = https.request(options, (res) => {
-      let data = "";
-      res.on("data", (chunk) => {
-        data += chunk;
+  try {
+    //通过正则匹配拿到token和port
+    const token = allData.match(/remoting-auth-token=(.*?)["'\s]/)[1];
+    const port = allData.match(/--app-port=(.*?)["'\s]/)[1];
+    const auth = Buffer.from(`riot:${token}`).toString("base64");
+    const headers = {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Basic ${auth}`,
+    };
+    const options = {
+      hostname: "127.0.0.1",
+      port: port,
+      path: url,
+      method: method,
+      headers: headers,
+      params: params,
+    };
+    return new Promise((resolve, reject) => {
+      const req = https.request(options, (res) => {
+        let data = "";
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+        res.on("end", () => {
+          resolve(data);
+        });
       });
-      res.on("end", () => {
-        resolve(data);
+      req.on("error", (error) => {
+        reject(error);
       });
+      req.end();
     });
-    req.on("error", (error) => {
-      reject(error);
-    });
-    req.end();
-  });
+  } catch {}
 };
 
 const getRiotData1 = () => {
@@ -84,7 +86,10 @@ const getRiotData1 = () => {
 const main = async () => {
   try {
     const leagueClientCommandline = await findLeagueClientCommandLine();
-    // console.log("LeagueClientUx.exe 命令行参数:", leagueClientCommandline);
+    console.log(
+      "LeagueClientUx.exe 命令行参数:",
+      typeof leagueClientCommandline
+    );
   } catch (error) {
     console.error("发生错误:", error);
   }
